179989e4fb2e80411dc114227f1d894837c630d1
Gui-Cang <821192514@qq.com>
2020-11-16 19:21:28 +0800

    [dingke] feat: finish practice2.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
-export { parseData };
+const parseData = (input) => {
+    let {data, column} = input;
+    const keyArr = column.map(e => e.name)
+    return data
+        .map(e => e
+            .reduce((acc, cur, curIndex) => {
+                acc[keyArr[curIndex]] = cur;
+                return acc
+            }, {})
+        )
+}
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
-export { inject };
+const inject = (arr, injectSections) => {
+    let result = arr;
+    let n = 0;
+    for (let e of injectSections) {
+        result.splice(e.index + n, 0, e.content);
+        n++;
+        // console.log(e.index,e.content)
+        // console.log(result)
+    }
+    return result;
+}
+export {inject};
